710b187a82d397644e62c438c0136edaf623bbe6
ethanPortman <C0523782@online.camosun.ca>
2023-09-15 16:54:26 -0700

    Created the Board Class

---
+++ Board.py
@@ -0,0 +1,10 @@
+
+class Board:
+    def __init__(self, length:int, num_treasures:int, min_treasure:int, max_treasure:int):
+        self.length = length
+        self.num_treasures = num_treasures
+        self.min_treasure = min_treasure
+        self.max_treasure = max_treasure
+
+
+my_board = Board()
